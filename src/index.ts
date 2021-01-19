c2acf6643cf60cd6eecc3cfa4b6fb7aea0e94236
vaaski <admin@vaa.ski>
2021-01-19 17:45:06 +0100

    remove extraneous "seconds"

--- src/index.ts
+++ src/index.ts
@@ -13,11 +13,11 @@ if (!token || !chat_id) {
 const formatDuration = (ms: number) => {
   if (ms < 0) ms = -ms
   const time = {
-    day: Math.floor(ms / 86400000),
-    hour: Math.floor(ms / 3600000) % 24,
-    minute: Math.floor(ms / 60000) % 60,
-    second: Math.floor(ms / 1000) % 60,
-    millisecond: Math.floor(ms) % 1000,
+    d: Math.floor(ms / 86400000),
+    h: Math.floor(ms / 3600000) % 24,
+    m: Math.floor(ms / 60000) % 60,
+    s: Math.floor(ms / 1000) % 60,
+    ms: Math.floor(ms) % 1000,
   }
   return Object.entries(time)
     .filter((val) => val[1] !== 0)
@@ -33,7 +33,7 @@ const deployMessage = (hook: NetlifyHook) => {
 
   return `☁️ successfully published <a href="${permalink}">new version</a> in ${formatDuration(
     deploy_time * 1e3,
-  )} seconds.`
+  )}.`
 }
 const deployError = ({ error_message }: NetlifyHook) => `🛑 deploy error:\n${error_message}`
 
